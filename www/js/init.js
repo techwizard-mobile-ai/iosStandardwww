7c74e76e82eccfd78e9d3cccb68d5b206d14b1d1
the-raccoondog <dylan1406lewis@gmail.com>
2014-09-16 11:07:00 -0400

    Documentation for JSONController and init.js

--- www/js/init.js
+++ www/js/init.js
@@ -1,4 +1,10 @@
 /*global $:false, document:false, JSONController:false, FormController:false */
+
+/**
+ * Set up controller classes and begin application flow
+ * @author Christopher Wilson
+ * @version 9/16/2014
+ */
 $(document).ready(function () {
 
     var jsonController = new JSONController(),
--- www/js/json-controller.js
+++ www/js/json-controller.js
@@ -1,6 +1,23 @@
 /*global $:false, document:false, console:false, alert:false */
+
+/**
+ * This class controls JSON requests to and from the main app server
+ * @author Christopher Wilson
+ * @version 9/16/2014
+ */
+
+/**
+ * Constructor for the JSONController class
+ * @param none
+ * @return none
+ */
 var JSONController = function () {
 
+    /**
+     * Request the list of substations from the app server
+     * @param none
+     * @return {Object} stations
+     */
     this.getStationList = function () {
     	var station_json,
     		stations;
@@ -20,7 +37,12 @@ var JSONController = function () {
 
         return stations;
     };
-
+    
+    /**
+     * Request a list of breakers from the selected substation
+     * @param {Object} event
+     * @return {Object} breaker_list
+     */
     this.getBreakerList = function (event) {
     	var breaker_json,
     		breaker_list;
@@ -41,7 +63,12 @@ var JSONController = function () {
 
         return breaker_list;
     };
-
+    
+    /**
+     * Request information on a specific breaker
+     * @param {Object} breaker
+     * @return {Object} breaker_info
+     */
     this.getBreakerInfo = function (breaker) {
     	var breaker_json,
             breaker_info;
@@ -62,7 +89,12 @@ var JSONController = function () {
 
         return breaker_info;
     };
-
+    
+    /**
+     * Request a list of regulators for the selected substation
+     * @param {Object} event
+     * @return {Object} regulator_list
+     */
     this.getRegulatorList = function (event) {
     	var regulator_json,
     		regulator_list;
@@ -83,7 +115,12 @@ var JSONController = function () {
 
         return regulator_list;
     };
-
+    
+    /**
+     * Request information for a specific regulator
+     * @param {Object} regulator
+     * @return {Object} regulator information
+     */
     this.getRegulatorInfo = function (regulator) {
     	var regulator_json,
             regulator_info;
