6f51dc90490d94ed5348e7aa667d84df267762db
the-raccoondog <dylan1406lewis@gmail.com>
2014-09-16 15:09:21 -0400

    Added db-controller.js
    Skeleton class

---
+++ www/js/db-controller.js
@@ -0,0 +1,16 @@
+/*global $:false, document:false, console:false, alert:false */
+
+/**
+ * This class controls local file storage in the event that the main app server cannot be reached
+ * @author Christopher Wilson
+ * @version 9/14/2014
+ */
+
+/**
+ * Constructor for the DBController class
+ * @param none
+ * @return none
+ */
+var DBController = function() {
+        
+};
