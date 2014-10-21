c4e5c21d0cb64cdeaee548d9de6e052ecdddf189
the-raccoondog <dylan1406lewis@gmail.com>
2014-10-21 14:47:16 -0400

    removed pointless models.js

--- www/js/models.js
+++
@@ -1,9 +0,0 @@
-/**
- * This file contains all the model objects used by the IndexedDB to store
- * substation information when an internet connection is not available
- */
-
-var SubStation = function (id, name) {
-    this.station_id = id;
-    this.station_name = name;
-};
